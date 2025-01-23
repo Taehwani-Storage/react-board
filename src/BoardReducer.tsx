1385e83c39d3bbacd8553b5dfedeb9e7f0d5f2d1
Taehwani-Storage <wiz_th1024@naver.com>
2025-01-23 17:49:50 +0900

    feat: TIL 0123 React Tutorial - 리액트 기본 문법 - 리액트 기본 구현 - 게시판 연동 (프론트 파트)

--- src/BoardReducer.tsx
+++ src/BoardReducer.tsx
@@ -1,7 +1,13 @@
 export function reducer(state, action) {
     switch (action.type) {
         case 'ON_LOAD':
-
+            return ({
+                list: action.list,
+                startPage: action.startPage,
+                endPage: action.endPage,
+                maxPage: action.maxPage,
+                currentPage: action.currentPge
+            })
 
         default:
             return state
